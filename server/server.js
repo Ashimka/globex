fd84c44d71a3855f3e3eac34a11f7b2df7a7bb44
Ashimka <ashimka2010@yandex.ru>
2024-06-01 00:31:16 +0500

    add: cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const fs = require("fs");
 const fastify = require("fastify")({ logger: true });
 
-fastify.register(require("fastify-cors"), {});
+fastify.register(require("fastify-cors"), {
+  origin: (origin, cb) => {
+    const hostname = new URL(origin).hostname;
+    if (hostname === "localhost") {
+      //  Request from localhost will pass
+      cb(null, true);
+      return;
+    }
+    // Generate an error on other origins, disabling access
+    cb(new Error("Not allowed"), false);
+  },
+});
 
-fastify.get("/", async (request, reply) => {
+fastify.get("/api", async (request, reply) => {
   fs.readFile("./users.json", "utf8", (err, data) => {
     if (err) {
       console.log("File read failed:", err);
